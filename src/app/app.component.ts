108f2201f95ca4b2978e1cbcc763649781c873b7
Allan <vredes@live.com>
2020-07-03 22:57:01 -0400

    completado 80%, funcional 100%

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+
+    personajes: string[] = ['Arnold Schwarzenegger', 'Sarah Connor', 'John Connor','T-1000'];
+    nombre    : string =   'Computación Cuántica';
+    arreglo   : number[] = [1,2,3,4,5,6,7,8,9,10];
+    PI        : number =   Math.PI;
+    porcentaje: number =   0.234;
+    salario   : number =   1234.5;
+    fecha     : Date = new Date();
+
+    idioma    : string = 'fr';
+
+    valorPromesa = new Promise<string>( (resolve) => {
+
+      setTimeout(() => {
+        resolve('llegó la data');
+      }, 4500);
+    });
+
+    heroe = {
+      nombre: 'Logan',
+      clave: 'Wolverine',
+      edad: 500,
+      direccion: {
+        calle: 'Primera',
+        casa: 20
+      }
+    }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
+
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es-CL';
+import localeEn from '@angular/common/locales/en';
+import localeFr from '@angular/common/locales/fr';
+
+registerLocaleData(localeEs, 'es-CL');
+registerLocaleData(localeEn, 'en');
+registerLocaleData(localeFr, 'fr');
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +19,12 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'es-CL'
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
